742cce5c22108a835bd92b32fa5bb237f92dff38
tamer <tamer.taji@gmail.com>
2024-05-07 21:36:11 +0300

    modify java script file by adding turns for two players & reset button functions and more ..

--- index.js
+++ index.js
@@ -10,40 +10,45 @@ document.getElementById("player1").textContent = firstName + " ";
 document.getElementById("player2").textContent = secondName + " ";
 
 
-var numCards = document.querySelectorAll(".card").length;
-var cards = document.querySelectorAll(".card");
-var twice = new Array(17).fill(0);
-
-
-//front
-for (let i = 0; i < numCards; i++) {
-  cards[i].classList.add("front");
-}
-
-
-//back
-for (let i = 0; i < numCards; i++) {
-  var randomNumber;
-
-  do {
-    randomNumber = Math.floor(Math.random() * 16) + 1;
-  } while (twice[randomNumber] === 2);
-    
-  twice[randomNumber]+=1;
-
-  var randomImageSource = "assests/" + randomNumber + ".jpg";
-  var img = document.createElement("img");
-  img.setAttribute("src", randomImageSource);
-
-  cards[i].appendChild(img);
-}
-
-
 var previousC = null; 
 var count = 0;
-var score = 0;
+var score1 = 0;
+var score2 = 0;
+var turn = 1;
 
-for (let i = 0; i < numCards; i++) {
+
+function setupGame() {
+
+  var numCards = document.querySelectorAll(".card").length;
+  var cards = document.querySelectorAll(".card");
+  var twice = new Array(17).fill(0);
+
+
+  //front
+  for (let i = 0; i < numCards; i++) {
+    cards[i].classList.add("front");
+  }
+
+
+  //back
+  for (let i = 0; i < numCards; i++) {
+    var randomNumber;
+
+    do {
+      randomNumber = Math.floor(Math.random() * 16) + 1;
+    } while (twice[randomNumber] === 2);
+      
+    twice[randomNumber]+=1;
+
+    var randomImageSource = "assests/" + randomNumber + ".jpg";
+    var img = document.createElement("img");
+    img.setAttribute("src", randomImageSource);
+
+    cards[i].appendChild(img);
+  }
+
+
+  for (let i = 0; i < numCards; i++) {
     document.querySelectorAll(".card")[i].addEventListener("click", function () {
 
       var currentC = this;
@@ -58,12 +63,32 @@ for (let i = 0; i < numCards; i++) {
 
       // Update previousC to the current card
       if(count === 2){
-        check(previousC,currentC);
+
+        playersTurn(previousC,currentC);
+
+        // check(previousC,currentC);
         previousC = null;
         count = 0;
       }
 
     });
+  }
+}
+
+
+function playersTurn(previous,current) {
+
+  if (turn==1) {
+
+    check(previous,current,1);
+    turn=2;
+
+  } else if (turn==2) {
+    check(previous,current,2);
+    turn=1;
+
+  }
+ 
 }
 
 
@@ -85,19 +110,63 @@ function clicked(currentKey){
 } 
 
 
-function check(c1,c2) {
+function check(c1,c2,pNum) {
   var card1 = c1.querySelector("img").getAttribute("src");
   var card2 = c2.querySelector("img").getAttribute("src");
 
+  function flipBack() {
+    return new Promise(resolve => {
+        setTimeout(function() {
+            c1.classList.remove("flipped");
+            c1.classList.add("front");
+            c2.classList.remove("flipped");
+            c2.classList.add("front");
+            resolve();
+        }, 1000);
+    });
+  }
+
+
   if (card1 === card2 && c1.id !== c2.id) {
-    score++;
+    if (pNum==1){
+     score1++;
+     document.getElementById("score" + pNum).textContent = score1;
+
+    }else if (pNum==2) {
+     score2++;
+     document.getElementById("score" + pNum).textContent = score2;
+    }
 
   }else{
-    setTimeout(function () {
-    c1.classList.remove("flipped");
-    c1.classList.add("front");
-    c2.classList.remove("flipped");
-    c2.classList.add("front");
-    }, 1000); 
+     flipBack().then(() => {
+            console.log("Cards flipped back over");
+        });
   }
 }
+
+
+setupGame();
+
+
+// Reset button
+document.getElementById('restartButton').addEventListener('click', function() {
+
+  score1 = 0;
+  score2 = 0;
+  document.getElementById("score" + 1).textContent = score1;
+  document.getElementById("score" + 2).textContent = score2;
+
+
+  // Remove existing cards children (images)
+  var cards = document.querySelectorAll(".card");
+  cards.forEach(card => {
+      while (card.firstChild) {
+          card.removeChild(card.firstChild);
+      }
+      card.classList.remove("flipped", "front", "pressed"); 
+      var cardClone = card.cloneNode(true); // true means it clones children as well
+      card.parentNode.replaceChild(cardClone, card);
+  });
+  
+  setupGame();  
+});
